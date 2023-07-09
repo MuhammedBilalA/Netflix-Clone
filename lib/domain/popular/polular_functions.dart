95bf77f062c07bdb30734035ea60fdb0f3b0874e
MuhammedBilalA <bilalmuhammed402@gmail.com>
2023-07-09 11:58:02 +0530

    Netflix Clone Completed

--- lib/domain/popular/polular_functions.dart
+++ lib/domain/popular/polular_functions.dart
@@ -4,7 +4,8 @@ import 'package:http/http.dart' as http;
 import 'package:netflix_clone/domain/popular/popular.dart';
 
 Future<List<Result>> getImagePopular() async {
-  final response = await http.get(Uri.parse(
+  final response = await http.get(
+    Uri.parse(
       'https://api.themoviedb.org/3/movie/popular?api_key=b7d1c1cf780d9b76cafd47071df61529&language=en-US&page=1'));
   if (response.statusCode == 200) {
     final jsondata = jsonDecode(response.body);
